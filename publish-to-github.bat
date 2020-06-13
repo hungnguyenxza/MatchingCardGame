94c81c4e9dd03f14831e59cfc2ded3bca4249663
Hùng Nguyễn <hungnx@pixelz.com>
2020-06-13 13:20:50 +0700

    update publish-to-github.cmd

--- publish-to-github.bat
+++
@@ -1,3 +0,0 @@
-ng build --prod --base-href https://nguyenxuanhung96.github.io/MatchingCardGame/
-
-angular-cli-ghpages -d dist/matching-card/ --no-silent
---
+++ publish-to-github.cmd
@@ -0,0 +1,1 @@
+ng build --prod --base-href https://nguyenxuanhung96.github.io/MatchingCardGame/ && angular-cli-ghpages -d dist/matching-card/ --no-silent && @pause
